f2d63627dc9084a08fa1e33b4da7cb235b81ec6b
Programmistochka <programmistochka@list.ru>
2023-03-04 01:45:23 +1200

    Add list all_finded_vacancy and description

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
+import pprint
 
 HOST = "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
 
@@ -8,22 +9,36 @@ def get_headers():
     return Headers(browser = "firefox", os = "win").generate()
 
 def get_text(url):
-    return requests.get(url, headers = get_headers()).text
+    return requests.get(url, headers = get_headers(), params={'count' : 50}).text
 
 
 if __name__ == "__main__":
     get_text(HOST)
+all_finded_vacancy = []
 
-    html_text=get_text(HOST)
+all_pages = range(2)
+for page in all_pages:
+    page_HOST = HOST + "&page=" + str(page) + "&items_on_page=50"
+    print('***********'+str(page)+'**************')
+    html_text=get_text(page_HOST)
     soup = BeautifulSoup(html_text, features ='lxml')
 
-    vacancy_bloks = soup.find('div', class_ = 'vacancy-serp-content').find_all('div', class_ = 'serp-item')
+    # pages = soup.find('a', {'data-qa': 'pager-next'})
+    # print(pages)
+    # pages = soup.find('a', {'class': 'pager'})
+    # print(pages)
+    # q_pages = soup.find('div', class_ = 'pager').get_text()
+    # print(q_pages)
+
+    vacancy_bloks = soup.find_all('div', class_ = 'serp-item')
     
     print(len(vacancy_bloks))
 
+   
+
     for vacancy in vacancy_bloks:
         print('-----')
-        print(vacancy)
+        #print(vacancy)
         print('-----')
         title = vacancy.find(class_ = 'serp-item__title').text
         print(title)
@@ -39,5 +54,23 @@ if __name__ == "__main__":
         else:
             zp = ('').join(zp.contents)
         print(zp)
+        description = vacancy.find('div', class_="g-user-content").text
+
+        all_finded_vacancy.append({
+            'title' :  title,
+            'link' : link,
+            'company' : company,
+            'city' : city,
+            'zp' : zp,
+            'description' : description
+        })
+print(all_finded_vacancy)
+print(len(all_finded_vacancy))
+    
+    #### Убрать символы ASCII  в зарплатах и дру полях вакансии, возможно необходимо взять полное описание вакансии
+    #а не краткое описание на главной страницы выборки
+
+    ### Как вычислить количество страниц и изменить количество вакансий выдаваемых в запросе
+
     
 
