1095fd07c2290df84c599a0afa607e72495683a0
Programmistochka <programmistochka@list.ru>
2023-04-19 15:20:23 +1200

    Work version

--- main.py
+++ main.py
@@ -9,68 +9,71 @@ def get_headers():
     return Headers(browser = "firefox", os = "win").generate()
 
 def get_text(url):
-    return requests.get(url, headers = get_headers(), params={'count' : 50}).text
+    params={'text': 'python', 'area' : 2, 'per_page':20}
+    #return requests.get('https://spb.hh.ru/search/vacancy', headers = get_headers(), params=params).text
+    return requests.get(url, headers = get_headers(), params = params).text
 
 
 if __name__ == "__main__":
-    get_text(HOST)
-all_finded_vacancy = []
+    #Получение текста html страницы
+    all_text=get_text(HOST)
+    soup = BeautifulSoup(all_text, features ='lxml')
+    all_finded_vacancy = []
 
-all_pages = range(2)
-for page in all_pages:
-    page_HOST = HOST + "&page=" + str(page) + "&items_on_page=50"
-    print('***********'+str(page)+'**************')
-    html_text=get_text(page_HOST)
-    soup = BeautifulSoup(html_text, features ='lxml')
-
-    # pages = soup.find('a', {'data-qa': 'pager-next'})
-    # print(pages)
-    # pages = soup.find('a', {'class': 'pager'})
-    # print(pages)
-    # q_pages = soup.find('div', class_ = 'pager').get_text()
-    # print(q_pages)
-
-    vacancy_bloks = soup.find_all('div', class_ = 'serp-item')
+    #Определение общего количества страниц по результатам запроса
+    all_pages = int(soup.find('div', attrs={"class":"pager"}).find_all("span", recursive=False)[-1].find("a").find("span").text)
+    print(f'По запросу найдено {all_pages}стр.')
     
-    print(len(vacancy_bloks))
+    #Обработка каждой страницы
+    for page in range(0, 5):
+        page_HOST = HOST + "&page=" + str(page)
+        html_text=get_text(page_HOST)
+        soup_page = BeautifulSoup(html_text, features ='lxml')
 
-   
+        #Получение блока с информацией по всем найденным на странице вакансиям
+        vacancy_bloks = soup_page.find_all('div', class_ = 'serp-item')
 
-    for vacancy in vacancy_bloks:
-        print('-----')
-        #print(vacancy)
-        print('-----')
-        title = vacancy.find(class_ = 'serp-item__title').text
-        print(title)
-        link =  vacancy.find(class_ = 'serp-item__title')['href']
-        print(link)
-        company = vacancy.find(class_='vacancy-serp-item__meta-info-company').text
-        print(company)
-        city = vacancy.find(class_='vacancy-serp-item__info').contents[1].text
-        print(city)
-        zp = vacancy.find(class_='vacancy-serp-item-body__main-info').find('span', class_='bloko-header-section-3')
-        if zp is None:
-            zp = "Зарплата не указана"
-        else:
-            zp = ('').join(zp.contents)
-        print(zp)
-        description = vacancy.find('div', class_="g-user-content").text
-
-        all_finded_vacancy.append({
-            'title' :  title,
-            'link' : link,
-            'company' : company,
-            'city' : city,
-            'zp' : zp,
-            'description' : description
-        })
-print(all_finded_vacancy)
-print(len(all_finded_vacancy))
+        #Определение параметров каждой найденной вакансии
+        for vacancy in vacancy_bloks:
+            print('-----')
+            title = vacancy.find(class_ = 'serp-item__title').text
+            print(title)
+            link =  vacancy.find(class_ = 'serp-item__title')['href']
+            print(link)
+            company = vacancy.find(class_='vacancy-serp-item__meta-info-company').text
+            print(company)
+            city = vacancy.find(class_='vacancy-serp-item__info').contents[1].text
+            print(city)
+            zp = vacancy.find(class_='vacancy-serp-item-body__main-info').find('span', class_='bloko-header-section-3')
+            if zp is None:
+                zp = "Зарплата не указана"
+            else:
+                zp = ('').join(zp.contents)
+                zp = zp.replace('\u202f','')
+            print(zp)
+            description = vacancy.find('div', class_="g-user-content")#.text
+            if description is None:
+                description = "Описания нет"
+            else:
+                description = vacancy.find('div', class_="g-user-content").text
+            
+            #Добавление информации по обработанной вакансии в результирующий словарь
+            all_finded_vacancy.append({
+                'title' :  title,
+                'link' : link,
+                'company' : company,
+                'city' : city,
+                'zp' : zp,
+                'description' : description
+            })
+    print('-------------------------------')
+    print(f'Всего обработано {len(all_finded_vacancy)} вакансий'
+    )
     
     #### Убрать символы ASCII  в зарплатах и дру полях вакансии, возможно необходимо взять полное описание вакансии
     #а не краткое описание на главной страницы выборки
 
-    ### Как вычислить количество страниц и изменить количество вакансий выдаваемых в запросе
+    
 
     
 
